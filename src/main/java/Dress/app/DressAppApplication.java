10e10eb93244834f77faa935ad31556d62d734e9
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-05-16 15:43:17 +0300

    Initial commit
    Initial commit

---
+++ src/main/java/Dress/app/DressAppApplication.java
@@ -0,0 +1,13 @@
+package Dress.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dress/app/DressAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Dress.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
