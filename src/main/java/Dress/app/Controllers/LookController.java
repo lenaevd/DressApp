e729029cdb636a71829a07c011369a4b91000350
lenaevd <lenciktulencik@gmail.com>
2024-06-14 18:08:38 +0300

    hochu na voleyball

--- src/main/java/Dress/app/Controllers/LookController.java
+++ src/main/java/Dress/app/Controllers/LookController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/looks")
 public class LookController {
